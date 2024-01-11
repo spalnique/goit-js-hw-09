99e2f48f06400796d47e7b9bcb8c03c24b20be69
spalnique <136997639+spalnique@users.noreply.github.com>
2024-01-11 21:54:03 +0000

    Deploying to gh-pages from @ spalnique/goit-js-hw-09-test-1@83405022efc8cdaf4d95e3117a8721f09eada941 🚀

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: 'src',
-    // build: {
-    //   outDir: '/dist',
-    // }
+    build: {
+      outDir: '../dist',
+    }
   });
