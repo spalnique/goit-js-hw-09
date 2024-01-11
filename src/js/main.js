c6c2b3130321df769a3e36b87c94916612601a19
Oleksii_S <lex.sydoruk@gmail.com>
2024-01-11 21:37:13 +0100

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import '../css/style.css'
+import javascriptLogo from '../../javascript.svg'
+import viteLogo from '../../public/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
