e7204fce6a4c251a816cc19d8b4d1a55c869ee6c
Oleksii_S <lex.sydoruk@gmail.com>
2024-01-11 23:11:30 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
+import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-    root: 'src',
-    build: {
-      outDir: '../dist',
-    }
-  });
+  define: {
+    _global: {},
+  },
+  root: 'src',
+  build: {
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+    },
+    outDir: '../dist',
+  },
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+});
