1bf101da87f0e7a44f417cb96d78acd947c0099e
Oleksii_S <lex.sydoruk@gmail.com>
2024-01-11 22:28:28 +0100

    sdgkl;'#dasdsa Your branch is up to date with 'goit-js-hw-09-test-1/main'.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
-import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
     root: 'src',
     build: {
-      outDir: '../dist',
-    },
-    plugins: [injectHTML()],
+      outDir: '../vladik',
+    }
   });
